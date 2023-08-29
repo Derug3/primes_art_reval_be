27d50e3c9b9b0152e43945005039f4ebf3f5d416
goran9999 <gutovicgoran@gmail.com>
2023-08-29 19:47:55 +0200

    -cron for recovering boxes -fixed webhook

--- src/recover_box/recover_box.module.ts
+++ src/recover_box/recover_box.module.ts
@@ -5,9 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { RecoverBox } from './entity/recover_box.entity';
 import { RecoverBoxRepository } from './recover_box.repository';
 import { SharedModule } from 'src/shared/shared.module';
-
+import { ScheduleModule } from '@nestjs/schedule';
 @Module({
-  imports: [TypeOrmModule.forFeature([RecoverBox]), SharedModule],
+  imports: [
+    TypeOrmModule.forFeature([RecoverBox]),
+    SharedModule,
+    ScheduleModule.forRoot(),
+  ],
   providers: [RecoverBoxResolver, RecoverBoxService, RecoverBoxRepository],
   exports: [RecoverBoxService],
 })
