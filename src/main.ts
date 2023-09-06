6340f9e1537e567ff26a8df5f3ba39a5286507b6
Ne-Lexa <alexey@nelexa.ru>
2023-09-06 10:36:23 +0300

    adds openapi config for api

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as basicAuth from 'express-basic-auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -25,6 +27,52 @@ async function bootstrap() {
   app.use(bodyParser.json({ limit: '10mb' }));
   app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+
+  // swagger
+  const swaggerUsername = process.env.SWAGGER_USERNAME as string;
+  const swaggerPassword = process.env.SWAGGER_PASSWORD as string;
+  if (swaggerUsername && swaggerPassword) {
+    const swaggerUsers: Record<string, string> = {
+      [swaggerUsername]: swaggerPassword,
+    };
+    app.use(
+      // Paths you want to protect with basic auth
+      '/api/doc',
+      basicAuth({
+        challenge: true,
+        users: swaggerUsers,
+      }),
+    );
+  }
+
+  const openApiConfig = new DocumentBuilder()
+    .setTitle('ThePrimes - ArtReveal')
+    .setDescription('The ArtReveal API')
+    .setVersion('1.0.0-' + process.env.NODE_ENV)
+    .addBasicAuth()
+    .addSecurity('admin-auth', {
+      type: 'apiKey',
+      description: 'Api Key',
+      name: 'X-Api-Key',
+      in: 'header',
+    })
+    .build();
+  const document = SwaggerModule.createDocument(app, openApiConfig, {
+    extraModels: [],
+  });
+  SwaggerModule.setup('api/doc', app, document, {
+    customCss: '.topbar{display: none} .swagger-ui textarea{font-size: 1.3rem}',
+    swaggerOptions: {
+      tryItOutEnabled: true,
+      displayOperationId: true,
+      defaultModelsExpandDepth: 0,
+      syntaxHighlight: {
+        theme: 'monokai',
+      },
+      persistAuthorization: true,
+    },
+  });
+
   await app.listen(process.env.PORT || 9000);
 }
 bootstrap();
